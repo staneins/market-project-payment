a3518d7d550b03257dd4dcb21a9846ec608f8156
Philip Kaminsky <kaminsky8836@gmail.com>
2024-12-17 19:20:02 +0300

    initial commit

---
+++ src/main/java/com/kaminsky/marketpayment/MarketPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.kaminsky.marketpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketPaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaminsky/marketpayment/MarketPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaminsky.marketpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
