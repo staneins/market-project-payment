63409b5ea5fdd7e5281e1fae10d72447c9806730
Philip Kaminsky <kaminsky8836@gmail.com>
2024-12-18 14:05:43 +0300

    minor fixes

--- src/main/java/com/kaminsky/marketpayment/config/ConsumerConfiguration.java
+++ src/main/java/com/kaminsky/marketpayment/config/ConsumerConfiguration.java
@@ -19,7 +19,7 @@ public class ConsumerConfiguration {
     public ConsumerFactory<String, PayedOrder> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "payed_group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, PayedOrder.class);
